1e2802375a240962b39516f4f21d03f066344914
dPliakos <dimitrispl96@gmail.com>
2018-07-17 23:59:18 +0300

    implement the bot

--- index.js
+++ index.js
@@ -6,31 +6,30 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 
 const bot = new SlackBot({
-  token: 'xoxb-7141114416-399444308178-qgEFQV7Cdujxsw7rTbffIQXR',
-  name: 'dr.update'
-})
-
-
-bot.on('start', ()=>{
-  const params ={
-    icon_emoju: ':smiley:'
-  }
-
-  // bot.postMessageToChannel('random', 'Test from bot!', params);
+  token: 'put the token here',
+  name: 'newsbot'
 });
 
 bot.on('message', payload=>{
   if (payload.type !== 'message') {
-    console.error('no message type');
     return;
   }
 
   var text = payload.text;
-  var hasUrl = text.indexOf('{url:') > 0;
-  // log(hasUrl)
+  var components = text.split('{url}');
 
-  bot.postMessageToChannel('bottest', payload.text);
+  var finalMessage = '';
+
+  if (components[0] && components[0].length > 0) {
+    finalMessage += `>>>${components[0]}`;
+  } 
+
+  if (components[1] && components[1].length && components[1].length > 0) {
+    finalMessage += `\n	${components[1]}`;
+  }
+
+  bot.postMessageToChannel('bottest', finalMessage);
   console.log(payload.text);
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('app listening on port 3000'))
